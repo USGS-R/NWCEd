4888d297cfe621bd032a8fb2b6d5448356eb4ddf
David Blodgett <dblodgett@usgs.gov>
2016-04-08 12:39:53 -0500

    Adding aggregate HUCs data.

--- R/getNWCData.R
+++ R/getNWCData.R
@@ -9,21 +9,34 @@
 #' @examples
 #' data<-getNWCData(huc="031601030306")
 #'
-getNWCData<-function(huc) {
+getNWCData<-function(huc, local=TRUE) {
   urls<-list(huc12=list(et="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_eta.nc",
-                        prcp="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_daymet.nc",
-                        MEAN_streamflow="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_Q.nc"))
-  if(nchar(huc)==12) {
+                        prcp="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_daymet.nc"),
+             huc12agg=list(et="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_eta_agg.nc",
+                        prcp="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_daymet_agg.nc",
+                        MEAN_streamflow="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC12_data/HUC12_Q.nc"),
+             huc08=list(et="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC08_data/HUC08_eta.nc",
+                        prcp="http://cida.usgs.gov/nwc/thredds/sos/watersmart/HUC08_data/HUC08_daymet.nc"))
+
+  if(nchar(huc)==12 && local) {
     urlList<-urls['huc12'][[1]]
+  } else if(nchar(huc)==12 && !local) {
+    urlList<-urls['huc12agg'][[1]]
+  } else if (nchar(huc)==8 && local) {
+    urlList<-urls['huc08'][[1]]
+  } else if (nchar(huc)==8 && !local) {
+    stop('Total upstream HUC08 watersheds are not available yet.')
   } else { # Will implement huc08 here too.
-    stop('must pass in a 12 - digit HUC identifier')
+    stop('must pass in an 8 or 12 digit HUC identifier')
   }
   dataOut<-list()
   for (name in names(urlList)) {
+    if(grepl(pattern = 'nwc/thredds/sos', x = urlList[name])) {
     url<-paste0(urlList[name],'?request=GetObservation&service=SOS&version=1.0.0&observedProperty=',
                 name,'&offering=',huc)
     # This is valid but not used now: ,'&eventTime=',startdate,'T00:00:00Z/', enddate,'T00:00:00Z'
     dataOut[name]<-list(getSWECSVBlock(url))
+    }
   }
   return(dataOut)
 }
--- tests/testthat/test_getNWCData.R
+++ tests/testthat/test_getNWCData.R
@@ -1,6 +1,10 @@
 context("Test Data Downloads")
 
-test_that("Test that the function runs without errors", {
+test_that("Test that the function returns the expected number of time series", {
   outData<-getNWCData("031601030306")
+  expect_equal(length(names(outData)),2)
+  outData<-getNWCData("031601030306", local=FALSE)
   expect_equal(length(names(outData)),3)
+  outData<-getNWCData("03160103")
+  expect_equal(length(names(outData)),2)
 })
