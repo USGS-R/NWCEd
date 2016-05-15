ac4617bc1b8d881da4ec48eb32ce9835c49dabed
David Blodgett <dblodgett@usgs.gov>
2016-05-15 14:38:50 -0500

    Implemented a function to get HUC12 geometry as geojson. Needed to switch to jsonlite in the process.

--- tests/testthat/test_getNWCData.R
+++ tests/testthat/test_getNWCData.R
@@ -14,3 +14,9 @@ test_that("Test that a HUC with NWIS data comes back as such.",{
   outData<-getNWCData("121101110705", local=FALSE)
   expect_equal(length(names(outData$streamflow)),5)
 })
+
+test_that('we get back some geojson when calling the getNWCWatershed function',{
+  outData<-getNWCWatershed("031601030306",local=TRUE)
+  expect_gt(length(outData$features[[1]]$geometry$coordinates[[1]][[1]]),100)
+  expect_equal(outData$type,"FeatureCollection")
+})
